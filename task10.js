1235ec712e1ade3e4d53baa5b217b5575c9f2822
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-07 22:03:47 +0200

    added ask 10" " " q git status

---
+++ task10.js
@@ -0,0 +1,15 @@
+//task 10
+ function vowelsPrint(string) {
+     const vowels = 'aeiou';
+     var phonetics = [];
+     for (var i = 0; i < string.length; i++) {
+     if (vowels.includes(string[i])) {
+     console.log(string[i])
+     } else {
+     phonetics += string[i] + '\n';
+     }
+     }
+console.log(phonetics);
+}
+
+vowelsPrint("makofanethulane");
