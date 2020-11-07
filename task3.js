13fee0c80d2d7742089c791ce671705a71d08267
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-07 21:43:55 +0200

    added task 33

---
+++ task3.js
@@ -0,0 +1,8 @@
+//task 3
+function Task3(x,y){
+return ((x == 65 || y == 65) || (x + y == 65)); 
+}
+// asking the user to insert the numbers 
+x = prompt( "please input  the x value ");
+y = prompt("please input  the y value ");
+console.log(Task3(x,y));
