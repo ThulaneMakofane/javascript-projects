11e0743e640b853fc12123be1f8990a4f10afe01
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-08 18:06:00 +0200

    make edits

--- task3.js
+++ task3.js
@@ -1,8 +1,9 @@
+
 //task 3
-function Task3(x,y){
+function sixtyFive(x,y){
 return ((x == 65 || y == 65) || (x + y == 65)); 
 }
 // asking the user to insert the numbers 
 x = prompt( "please input  the x value ");
 y = prompt("please input  the y value ");
-console.log(Task3(x,y));
+console.log(sixtyFive(x,y));
