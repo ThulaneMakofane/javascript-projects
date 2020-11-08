0a4e122b2c8a7e6c48885a4ff3dc6d5c66583567
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-08 18:08:47 +0200

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,10 +1,7 @@
-//task 4
-function Task4(num1 ,num2){
-
+// task 4
+function numThree(num1 ,num2){
 let sum = num1 + num2;
 return(((sum +"").includes('3')) && (num1==3 || num2==3))// {
-
-
 }
 
-console.log(Task4(5,10));
+console.log(numThree4(5,10));
