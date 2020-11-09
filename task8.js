d35919c13f15b6fa4c31c4d0600acd6ad776e4e9
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-09 19:05:24 +0200

    Update task8.js
    fixed the function so that it can work with plurals

--- task8.js
+++ task8.js
@@ -1,11 +1,18 @@
-//task 8
-
 function realTime(num)
  { 
   var hours = Math.floor(num / 60);  
   var minutes = num % 60;
-  //return hours + ":" + minutes;         
-  console.log(num + " will become" + hours+  "hour" + "and" + minutes + " minutes")
-}
+     
+  if(hours >1 && minutes >1 ){
+   console.log(num + " will become" + hours+  "hours" + "and" + minutes + " minutes");
+  }else if(hours<=1 && minutes<=1 ){
+console.log(num + " will become" + hours+  "hour" + "and" + minutes + " minute");
+  }else if(hours>1 && minutes<=1){
+    console.log(num + " will become" + hours+  "hours" + "and" + minutes + " minute");
+  }else if(hours<=1 && minutes>1){
+    console.log(num + " will become" + hours+  "hour" + "and" + minutes + " minutes");
+  }
+ }
+realTime(123);
 
-realTme(71);
+
