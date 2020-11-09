73a15078d01a55b6e2ea56af0920fdde6b8953e7
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-09 18:04:51 +0200

    Update task7.js

--- task7.js
+++ task7.js
@@ -1,17 +1,17 @@
 //task 7
 
-function CElsiusToFahrenheits(celsius) 
+function cElsiusToFahrenheits(celsius) 
 {
   var celsiusTemp = celsius;
-  var CelsiusToFahr = celsiusTemp * 9 / 5 + 32;
-    console.log(celsiusTemp+'\xB0C is ' + CelsiusToFahr + ' \xB0F.');
+  var celsiusToFahr = celsiusTemp * 9 / 5 + 32;
+    console.log(celsiusTemp+'\xB0C is ' + celsiusToFahr + ' \xB0F.');
 }
 
 function fahrenheitsToCelsius(fahrenheit) 
 {
   var fahrenheitsTemp = fahrenheit;
-  var fahrenheitsTemptoCelcius = (fahrenheitsTemp - 32) * 5 / 9;
-    console.log( fahrenheitsTemp+'\xB0F is ' + fahrenheitsTemptoCelcius + '\xB0C.');
+  var fahrenheitsTempToCelcius = (fahrenheitsTemp - 32) * 5 / 9;
+    console.log( fahrenheitsTemp+'\xB0F is ' + fahrenheitsTempToCelcius + '\xB0C.');
 } 
-CElsiusToFahrenheits(20);
+cElsiusToFahrenheits(20);
 fahrenheitsToCelsius(100);
