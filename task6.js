442830a8c57fe47eafb756cecbb4aea97cf3b98a
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-10 19:10:05 +0200

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,24 +1,21 @@
 //task 6
 
-function largestNum(){
-
-    let result ="";
-   var num1 =6;
-   var num2 = 7;
-   var num3 =19;
+function largestNum(num1,num2,num3){
+ 
   
     if(num1>num2 && num1>num3)
 		{
-			return result =console.log(num1+" is  the maximum number");
+			return num1;
 		}
 		else if(num2>num1 && num2>num3)
 		{
-		return  result=	console.log(num2 +" is  the maximum number");
+		return num2;
 		}
 		else if(num3>num1 && num3>num1)
 		{
-       return result=console.log(num3+" is the maximum number");
+       return num3;
     }
    
 }
-largestNum();
+largestNum(7,8,9);
+
