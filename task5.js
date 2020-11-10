ca20f838b092319bc2713b06376b3fb3da4f0e38
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-10 18:58:00 +0200

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,11 +1,8 @@
 // task 5
-function areaCalculations(){
- var side1= 2;
- var side2=2;
- var side3=2;
+function areaCalculations(side1,side2,side3){
 
 const parameter = (side1 +side2 + side3 ) /2;
 const area = Math.sqrt(parameter * (parameter- side1) * (parameter - side2)* (parameter-side3));
 return area;
 }
-console.log(" The area of this triangle is "+ areaCalculations());
+areaCalculations(2,2,2);
