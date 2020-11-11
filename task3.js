331adca7f8c69ff3d5818b6c9df9e06d76a2ef23
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-11 16:51:47 +0200

    Update task3.js

--- task3.js
+++ task3.js
@@ -3,7 +3,4 @@
 function sixtyFive(x,y){
 return ((x == 65 || y == 65) || (x + y == 65)); 
 }
-// asking the user to insert the numbers 
-x = prompt( "please input  the x value ");
-y = prompt("please input  the y value ");
-console.log(sixtyFive(x,y));
+sixtyFive(65,4);
