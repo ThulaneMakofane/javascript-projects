785b16e35f521505169471957b90c146cad84503
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-12 17:53:21 +0200

    Update task8.js

--- task8.js
+++ task8.js
@@ -1,17 +1,18 @@
+  
 function realTime(num)
  { 
   var hours = Math.floor(num / 60);  
   var minutes = num % 60;
      
   if(hours >1 && minutes >1 ){
-   console.log(num + " will become" +" "+ hours+ " "+ "hours"  +" "+"and" +" "+ minutes + " minutes");
+   console.log(  hours +" " +"hours"+ " ,"  + minutes + " minutes.");
   }else if(hours<=1 && minutes<=1 ){
-console.log(num + " will become" +" "+ hours+ " "+ "hour" + " " + "and" +" "+ minutes + " minute");
+console.log( hours+ " "+ "hour" + ", " +  +" "+ minutes + " minute");
   }else if(hours>1 && minutes<=1){
-    console.log(num + " will become" + " "+hours+ " "+ "hours" + " "+"and" +" " + minutes + " minute");
+    console.log(hours+ " "+ "hours" + ", "+ minutes + " minute");
   }else if(hours<=1 && minutes>1){
-    console.log(num + " will become" +" "+ hours+ " "+ "hour" + " " +"and" + " "+ minutes + " minutes");
+    console.log( hours+ " "+ "hour" + " , "+ " "+ minutes + " minutes");
   }
  }
-realTime(123);
+realTime(789)
 
