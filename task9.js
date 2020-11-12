01e4f9fc0d3bd20596188174bd5ca398970f19cc
ThulaneMakofane <makofanethulane53@gmail.com>
2020-11-12 17:58:49 +0200

    Update task9.js

--- task9.js
+++ task9.js
@@ -1,10 +1,12 @@
 function sumOfMultiples() {
     let sum = 0;
-    for(let i=1; i<10; i++) {
+    for(let i=1; i<1000; i++) {
         if(i % 3 === 0 || i % 5 === 0){
             sum += i;
         }
         
     }
     return sum;
+    
 }
+console.log(sumOfMultiples());
